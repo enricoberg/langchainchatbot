51b77b1efde71a11a623925cbe4f5a249d4a6338
Enrico <enricobergamini@outlook.it>
2024-08-27 14:08:17 +0200

    First issue

--- README.md
+++ README.md
@@ -1,2 +1,231 @@
-# langchainchatbot
-An AI chatbot configured to work with both Gemini and OpenAI APIs.
+# Langchain Chatbot
+
+This bot was built to be able to assist newly hired employees to learn and navigate corporate procedures.
+The program though can be used to build custom bots that use several PDF files as source of their knowledge.
+The app is preconfigured to work with the following models:
++ AzureChatOpenAI (default)
++ ChatGoogleGenerativeAI
+
+Currently, the OpenAI APIs do not have a free subscription plan for personal use.
+You can still play for free though, using the Gemini APIs with your personal account.
+
+[Click here to request your free Google API Key](https://makersuite.google.com/app/apikey?hl=it)
+## Installation
+1. First you need to have Python installed
+[Download it here](https://www.python.org/downloads/)
+
+2. Install all the required packages:
+	```
+	pip install -r requirements.txt
+	```
+3. Fill in your API credentials in the .env file (see .env.example)
+4. Run the app with
+	```
+	streamlit run app.py
+	```
+By default, the app uses the Azure OpenAI model, but you can switch to Gemini adding the flag
+		```
+		  --gemini
+		```
+	You can choose to visualize the sidebar that lets you add PDFs to the vector database by using the flag
+		```
+	--showsidebar
+	```
+	By default the sidebar is hidden so that the user can only access a predefined set of files.
+## Contributing
+
+  
+
+  
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+  
+
+  
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+
+  
+
+Don't forget to give the project a star! Thanks again!
+
+  
+
+  
+
+1. Fork the Project
+
+  
+
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+
+  
+
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+
+  
+
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+
+  
+
+5. Open a Pull Request
+
+  
+
+  
+  
+  
+
+  
+  
+  
+
+  
+
+<!-- CONTACT -->
+
+  
+
+## Contact
+
+  
+
+  
+
+Enrico Bergamini - enricobergamini@outlook.it
+
+  
+
+[![LinkedIn][linkedin-shield]][linkedin-url]
+
+
+  
+  
+
+<!-- MARKDOWN LINKS & IMAGES -->
+
+  
+
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+
+  
+
+[contributors-shield]: https://img.shields.io/github/contributors/othneildrew/Best-README-Template.svg?style=for-the-badge
+
+  
+
+[contributors-url]: https://github.com/othneildrew/Best-README-Template/graphs/contributors
+
+  
+
+[forks-shield]: https://img.shields.io/github/forks/othneildrew/Best-README-Template.svg?style=for-the-badge
+
+  
+
+[forks-url]: https://github.com/othneildrew/Best-README-Template/network/members
+
+  
+
+[stars-shield]: https://img.shields.io/github/stars/othneildrew/Best-README-Template.svg?style=for-the-badge
+
+  
+
+[stars-url]: https://github.com/othneildrew/Best-README-Template/stargazers
+
+  
+
+[issues-shield]: https://img.shields.io/github/issues/othneildrew/Best-README-Template.svg?style=for-the-badge
+
+[HTML-url]: https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=whit
+
+[issues-url]: https://github.com/othneildrew/Best-README-Template/issues
+
+  
+
+[license-shield]: https://img.shields.io/github/license/othneildrew/Best-README-Template.svg?style=for-the-badge
+
+  
+
+[license-url]: https://github.com/othneildrew/Best-README-Template/blob/master/LICENSE.txt
+
+  
+
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+
+  
+
+[linkedin-url]: https://linkedin.com/in/enrico-bergamini
+
+  
+
+[product-screenshot]: images/screenshot.png
+
+  
+
+[Next.js]: https://img.shields.io/badge/next.js-000000?style=for-the-badge&logo=nextdotjs&logoColor=white
+
+  
+
+[Next-url]: https://nextjs.org/
+
+  
+
+[React.js]: https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB
+
+  
+
+[React-url]: https://reactjs.org/
+
+  
+
+[Vue.js]: https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vuedotjs&logoColor=4FC08D
+
+  
+
+[Vue-url]: https://vuejs.org/
+
+  
+
+[Angular.io]: https://img.shields.io/badge/Angular-DD0031?style=for-the-badge&logo=angular&logoColor=white
+
+  
+
+[Angular-url]: https://angular.io/
+
+  
+
+[Svelte.dev]: https://img.shields.io/badge/Svelte-4A4A55?style=for-the-badge&logo=svelte&logoColor=FF3E00
+
+  
+
+[Svelte-url]: https://svelte.dev/
+
+  
+
+[Laravel.com]: https://img.shields.io/badge/Laravel-FF2D20?style=for-the-badge&logo=laravel&logoColor=white
+
+  
+
+[Laravel-url]: https://laravel.com
+
+  
+
+[Bootstrap.com]: https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white
+
+  
+
+[Bootstrap-url]: https://getbootstrap.com
+
+  
+
+[JQuery.com]: https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white
+
+  
+
+[JQuery-url]: https://jquery.com
+
+[CSS-url]: https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=whit
+
+[JAVASCRIPT-url]: https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black
---
+++ app.py
@@ -0,0 +1,140 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain.chat_models import AzureChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+
+#LOAD ALL THE CREDENTIALS FROM THE ENV FILE
+load_dotenv()
+GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+API_CLIENT=os.getenv("API_CLIENT")
+API_SECRET=os.getenv("API_SECRET")
+API_BASE_URL=os.getenv("API_BASE_URL")
+open_api_headers={
+  "client_id": API_CLIENT,
+  "client_secret": API_SECRET
+}
+open_api_base=API_BASE_URL
+os.environ["OPENAI_API_TYPE"] = "azure"
+os.environ["OPENAI_API_BASE"] = open_api_base
+os.environ["OPENAI_API_VERSION"] = "2023-03-15-preview"
+
+
+
+def get_pdf_text(pdf_docs):    
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
+
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vector_store(text_chunks):
+    #CREATE EMBEDDINGS USING THE DIFFENTE CLASSES SPECIFIC TO GEMINI AND AZURE
+    if "--gemini" in st.cli.argv:
+        embeddings = GoogleGenerativeAIEmbeddings(model = "models/text-embedding-004")
+    else:
+        embeddings=OpenAIEmbeddings(openai_api_base=API_BASE_URL,headers=open_api_headers, deployment="text-embedding-ada-002",model="text-embedding-ada-002", openai_api_key="not relevant", chunk_size=1, openai_api_type="azure", openai_api_version="2023-03-15-preview")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+
+def get_conversational_chain():
+
+    prompt_template = """    
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "Mi dispiace, non ho trovato una risposta nelle procedure  caricate a sistema...". don't provide the wrong answer. The answer MUST be in italian all the times.
+    Always specifiy if the answer is not coming from the context, otherwise cite the source of the information\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+    if "--gemini" in st.cli.argv:
+        model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.3)
+    else:
+        model = AzureChatOpenAI(
+        openai_api_base=open_api_base,
+        openai_api_version="2023-03-15-preview",                      
+        deployment_name="chatgpt4-32k",                                
+        openai_api_key="not_relevant",
+        headers = open_api_headers,
+        max_tokens =8096,                                             
+        temperature= 0.6)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    return chain
+
+
+
+def user_input(user_question):
+    if "--gemini" in st.cli.argv:
+        embeddings= GoogleGenerativeAIEmbeddings(model = "models/text-embedding-004")
+    else:
+        embeddings=OpenAIEmbeddings(openai_api_base=API_BASE_URL,headers=open_api_headers, deployment="text-embedding-ada-002",model="text-embedding-ada-002", openai_api_key="not relevant", chunk_size=1, openai_api_type="azure", openai_api_version="2023-03-15-preview")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+    chain = get_conversational_chain()    
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+    #PRINT THE RESPONSE IN THE TEXTBOX
+    st.write(":blue[B|Buddy: ]", response["output_text"])
+
+
+
+
+def main(show_sidebar):
+    
+    
+    
+    st.set_page_config("Chat PDF")
+    st.header("Chatta con B|Buddy 🤖")
+    
+
+    user_question = st.text_input("Chiedimi qualunque cosa, io conosco tutte le procedure BAI e sono qui per aiutarti:")
+
+    if user_question:
+        user_input(user_question)
+    if show_sidebar:
+        with st.sidebar:
+            st.title("Menu:")
+            pdf_docs = st.file_uploader("Carica qui i tuoi file e premi Apprendi", accept_multiple_files=True)
+            if st.button("Apprendi"):
+                with st.spinner("Studiando..."):
+                    raw_text = get_pdf_text(pdf_docs)
+                    text_chunks = get_text_chunks(raw_text)
+                    get_vector_store(text_chunks)
+                    st.success("Fatto")
+
+
+
+if __name__ == "__main__":
+    show_sidebar=False
+    if "--openai" in st.cli.argv:
+        print("Using OpenAI model")
+    elif "--gemini" in st.cli.argv:
+        print("Using Gemini model")
+    if "--showsidebar" in st.cli.argv:
+        show_sidebar=True
+
+    main(show_sidebar)
